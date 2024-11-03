e82d547e15d4d731959714a0c7109fd6e7336247
Ank222001 <u20181h103@upc.edu.pe>
2024-11-03 14:06:49 -0500

    feat: add model profile

---
+++ FlavourTrip/src/app/components/profile/model/usuario.entity.ts
@@ -0,0 +1,20 @@
+export class Usuario {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  birthdate: string;
+  location: string;
+  suscription: string;
+  constructor() {
+    this.id = 0;
+    this.firstName = "";
+    this.lastName = "";
+    this.email = "";
+    this.phone = "";
+    this.birthdate = "";
+    this.location = "";
+    this.suscription = "";
+  }
+}
